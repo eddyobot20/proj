ad3ffd5b23ea33975290f53cde6620d60a890363
eddyobot20 <edidiongobot1410@gmail.com>
2025-10-03 12:09:24 +0100

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// alert (
-//     "Welcome To Naija Dish Hub. Please SIgn In to get full access to all our recipes. Thank You!"
-// )
+alert (
+    "Welcome To Naija Dish Hub. Please SIgn In to get full access to all our recipes. Thank You!"
+)
 
