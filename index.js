a75fccb525d2229819c35d25aaaf0969cee81b25
eddyobot20 <edidiongobot1410@gmail.com>
2025-10-31 01:38:41 +0100

    changes in photos, background and links

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ alert (
     "Welcome To Naija Dish Hub. Please SIgn In to get full access to all our recipes. Thank You!"
 )
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
